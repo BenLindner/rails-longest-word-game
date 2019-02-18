dd22db52b05a5ccb8fcda46a47e2f28936a06190
Ben Lindner <lindnerben@icloud.com>
2019-02-18 18:06:53 +1100

    ..

---
+++ app/controllers/games_controller.rb
@@ -0,0 +1,30 @@
+require "open-uri"
+
+class GamesController < ApplicationController
+
+  def new
+    @letter_array = []
+    10.times do
+      @letter_array << ('a'..'z').to_a.sample
+    end
+  end
+
+  def score
+    @letters = params[:letters].split
+    @word = (params[:word] || "").upcase
+    @included = included?(@word, @letters)
+    @english_word = english_word?(@word)
+  end
+
+  private
+
+  def included?(word, letters)
+    word.chars.all? { |letter| word.count(letter) <= letters.count(letter) }
+  end
+
+  def english_word?(word)
+    response = open("https://wagon-dictionary.herokuapp.com/#{word}")
+    json = JSON.parse(response.read)
+    json['found']
+  end
+end
